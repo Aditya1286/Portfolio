db3b57167e16d4156c89388b101de5a36834159d
Aditya Aishwarya <aishwaryaaditya2@gmail.com>
2025-07-16 10:41:28 +0530

    added lenis

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useEffect } from "react"
 import Hero from "../components/Hero"
 import Grid from "../components/Grid"
 import Cert from "../components/Cert"
@@ -7,11 +10,33 @@ import Education from "../components/Education"
 import { FloatingNav } from "../components/ui/FloatingNavbar"
 import Footer from "../components/Footer"
 import { navItems } from "../data/index"
+import Lenis from "lenis"
+
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis({
+  duration: 1,
+  easing: (t) => Math.sin((t * Math.PI) / 2), // smooth easeOutSine
+  touchMultiplier: 2,
+  wheelMultiplier: 1.2,
+
+})
+    const raf = (time:number) => {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     <main className="relative bg-black text-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-       <FloatingNav navItems={navItems} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <Education />
@@ -21,5 +46,5 @@ export default function Home() {
         <Footer />
       </div>
     </main>
-  );
+  )
 }
